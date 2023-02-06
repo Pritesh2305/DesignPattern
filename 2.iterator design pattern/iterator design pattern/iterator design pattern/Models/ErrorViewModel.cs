a4d2bb0d9071a5efd83a2e238d6ffc38b6c9c50e
Pritesh Bhavsar <bhavsarpritesh.mca@gmail.com>
2023-02-06 18:30:32 +0530

    create factorydesignclass

---
+++ 2.iterator design pattern/iterator design pattern/iterator design pattern/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iterator_design_pattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 3.Factory Design Pattern/Factory Design Pattern/Factory Design Pattern/classdemo/FactoryDesign.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Factory_Design_Pattern.classdemo
+{
+    public interface ICar
+    {
+        void start();
+    }
+    public class SixSeatercar : ICar
+    {
+        public void start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class SevenSeatercar : ICar
+    {
+        public void start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CarFactory
+    {
+        public ICar GetCar(string cartype)
+        {
+            switch (cartype)
+            {
+                case "SixSeatercar":
+                    return new SixSeatercar();
+                case "SevenSeatercar":
+                    return new SevenSeatercar();
+            }
+            return null;
+        }
+    }
+}
