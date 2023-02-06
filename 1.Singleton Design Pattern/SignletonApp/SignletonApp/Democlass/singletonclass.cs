0eef4a6958e3973cdf3696f0721e1b8710ceac0d
Pritesh Bhavsar <bhavsarpritesh.mca@gmail.com>
2023-02-06 16:11:44 +0530

    create singletonclass

---
+++ 1.Singleton Design Pattern/SignletonApp/SignletonApp/Democlass/singletonclass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignletonApp.Democlass
+{
+    public sealed class singletonclass
+    {
+        private static singletonclass instance;
+        private static object obj;
+        private singletonclass() { }
+        public static singletonclass GetInstance()
+        {
+            lock (obj)
+            {
+                if (instance == null)
+                {
+                    instance = new singletonclass();
+                }
+                return instance;
+            }
+        }        
+    }
+}
+
